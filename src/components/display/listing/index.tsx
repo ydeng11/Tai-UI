cea37ca7a29f70e4ce21cc87c3e050d90b4b84d5
ydeng <dengyujun11@gmail.com>
2024-07-04 22:07:00 -0400

    feat: use state for last fetched todos

--- src/components/display/listing/index.tsx
+++ src/components/display/listing/index.tsx
@@ -1,12 +1,20 @@
 import TodoItem from "@/components/display/todoItem";
 import useStore from "@/components/stores/todoStore";
 import {useTodosQuery} from "@/components/hooks";
+import {useEffect, useState} from "react";
+import {Ttodo} from "@/components/types";
 
 const TodoList = () => {
     const { data } = useTodosQuery()
+    const [todos, setTodos] = useState<Ttodo[]>([]);
+    useEffect(() => {
+        if (data) {
+            setTodos(data);
+        }
+    }, [data]);
 
     const {categoryFilter, hashtagFilter} = useStore();
-    const filteredTodos = data
+    const filteredTodos = todos
         .filter((todo) => (categoryFilter.category === 'All' || todo.category.toLowerCase() === categoryFilter.category.toLowerCase()))
         .filter((todo) => (hashtagFilter.hashtag === 'All' || todo.hashtags.includes(hashtagFilter.hashtag.toLowerCase())))
         .filter((todo) => todo.isDeleted === 0);
